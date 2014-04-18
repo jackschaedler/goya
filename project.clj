8008ceb24595448a7f1ae8ec01888c09690b659b
David Nolen <david.nolen@gmail.com>
2014-04-18 12:38:13 -0400

    Project layout refactor, better production settings
    The project layout did not follow Java classpath conventions. The
    project source filenames did not correspond to declared namespaces.
    
    Bump Om dependency.
    
    Use react_with_addons.js as the externs file, disable noisy Google
    Closure warnings. Cleanup index.html to only include the final
    production main.js. The total gzipped JS payload for Goya is now 75K.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
-                 [om "0.5.1"]
+                 [om "0.5.3"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
   :plugins [[lein-cljsbuild "1.0.2"]]
   :cljsbuild {
@@ -15,5 +15,7 @@
           :output-to "resources/main.js"
           :optimizations :advanced
           :pretty-print false
-          :preamble ["react/react.min.js"]
-          :externs ["react/externs/react.js"]}}]})
+          :preamble ["react/react_with_addons.min.js"]
+          :externs ["react/react_with_addons.js"]
+          :closure-warnings {:externs-validation :off
+                             :non-standard-jsdoc :off}}}]})
