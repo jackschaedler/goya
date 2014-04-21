c24f284e1375606673f048e200c538a5bdd764b3
Jack Schaedler <jwschaedler@gmail.com>
2014-04-21 20:45:15 +0200

    Show progress indication when exporting history animation

--- src/cljs/goya/components/mainmenu.cljs
+++ src/cljs/goya/components/mainmenu.cljs
@@ -8,6 +8,10 @@
             [cljs.core.async :refer [put! chan <! alts!]]))
 
 
+(defn show-progress [progress owner]
+  (let [p (.round js/Math (* progress 100))]
+    (om/set-state! owner :progress p)))
+
 (defn dump-image []
   (.log js/console (str (get-in @app/app-state [:main-app :image-data]))))
 
@@ -18,13 +22,13 @@
     (.click download-link)))
 
 
-(defn download-history-animation [blob]
+(defn download-history-animation [blob owner]
   (let [download-link (. js/document (getElementById "image-download-link"))]
-    (println "done!")
+    (om/set-state! owner :is-processing false)
     (set! (.-href download-link) (.createObjectURL js/URL blob))
     (.click download-link)))
 
-(defn export-history-animation []
+(defn export-history-animation [owner]
   (let [render-canvas (. js/document (createElement "canvas"))
         width (get-in @app/app-state [:main-app :canvas-width])
         height (get-in @app/app-state [:main-app :canvas-height])
@@ -35,13 +39,16 @@
                           :width (* width zoom-factor)
                           :height (* height zoom-factor)
                           :workerScript "./gifjs/dist/gif.worker.js"})]
+    (om/set-state! owner :is-processing true)
+    (om/set-state! owner :progress 0)
     (dotimes [x (count app-history)]
       (let [history-snapshot (nth app-history x)
             image-data (get-in history-snapshot [:image-data])
             context (.getContext render-canvas "2d")]
         (canvasdrawing/draw-image-to-canvas image-data render-canvas width height zoom-factor)
         (.addFrame gif context #js {:copy true :delay 300})))
-    (.on gif "finished" #(download-history-animation %))
+    (.on gif "finished" #(download-history-animation % owner))
+    (.on gif "progress" #(show-progress % owner))
     (.render gif)))
 
 
@@ -64,13 +71,22 @@
 
 (defn menu-entry-component [entry owner]
   (reify
+    om/IInitState
+      (init-state [_]
+        {:is-processing false
+         :progress 0})
+
     om/IRenderState
       (render-state [this {:keys [clickchan]}]
         (omdom/div
           #js {:className "main-menu-item"
-               :onClick (fn [e] (put! clickchan (:command @entry)))}
+               :onClick (fn [e] (put! clickchan {:command (:command @entry)
+                                                 :owner owner}))}
           (omdom/i #js {:className (:icon entry)})
-          (:text entry)))))
+          (if
+            (om/get-state owner :is-processing)
+            (str "Working... "(om/get-state owner :progress) "%")
+            (:text entry))))))
 
 
 (defn menu-component [app owner]
@@ -83,10 +99,12 @@
     (will-mount [_]
        (let [clickchan (om/get-state owner :clickchan)]
         (go (loop []
-          (let [e (<! clickchan)]
-            (when (= e :new-doc) (create-new-document app owner))
-            (when (= e :export-doc) (export-image) (dump-image))
-            (when (= e :export-history-animation) (export-history-animation))
+          (let [e (<! clickchan)
+                command (:command e)
+                origin-owner (:owner e)]
+            (when (= command :new-doc) (create-new-document app owner))
+            (when (= command :export-doc) (export-image) (dump-image))
+            (when (= command :export-history-animation) (export-history-animation origin-owner))
           (recur))))))
 
     om/IRenderState
