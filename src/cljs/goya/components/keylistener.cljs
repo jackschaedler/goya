24491e6c02ea9d4afa55e4f7ce6f3af4587bd5a3
Jack Schaedler <jwschaedler@gmail.com>
2014-06-03 20:49:54 +0200

    Hook up key shortcuts for animation controls

--- src/cljs/goya/components/keylistener.cljs
+++ src/cljs/goya/components/keylistener.cljs
@@ -1,6 +1,7 @@
 (ns goya.components.keylistener
   (:require-macros [cljs.core.async.macros :refer [go]])
 	(:require [goya.components.canvastools :as canvastools]
+            [goya.components.animation :as animation]
             [goya.timemachine :as timemachine]
             [om.core :as om :include-macros true]
             [om.dom :as omdom :include-macros true]
@@ -13,14 +14,19 @@
 (def FOUR-KEY 52)
 (def FIVE-KEY 53)
 (def SIX-KEY 54)
+(def A-KEY 65)
+(def D-KEY 68)
 (def C-KEY 67)
 (def G-KEY 71)
 (def Z-KEY 90)
 (def X-KEY 88)
 (def Q-KEY 81)
 (def W-KEY 87)
+(def O-KEY 79)
 (def MINUS-KEY 189)
 (def PLUS-KEY 187)
+(def LEFT-ARROW-KEY 37)
+(def RIGHT-ARROW-KEY 39)
 
 
 (def key-chan (chan))
@@ -38,12 +44,17 @@
                    (= keyCode G-KEY) #(canvastools/toggle-grid app)
                    (= keyCode W-KEY) #(canvastools/zoom-in app)
                    (= keyCode Q-KEY) #(canvastools/zoom-out app)
+                   (= keyCode O-KEY) #(animation/toggle-onion-skin app)
+                   (= keyCode A-KEY) #(animation/add-new-frame app)
+                   (= keyCode D-KEY) #(animation/delete-current-frame app)
                    (= keyCode ONE-KEY) #(select-tool app :brush)
                    (= keyCode TWO-KEY) #(select-tool app :line)
                    (= keyCode THREE-KEY) #(select-tool app :box)
                    (= keyCode FOUR-KEY) #(select-tool app :fill)
                    (= keyCode FIVE-KEY) #(select-tool app :picker)
                    (= keyCode SIX-KEY) #(select-tool app :selection)
+                   (= keyCode LEFT-ARROW-KEY) #(animation/prev-frame app)
+                   (= keyCode RIGHT-ARROW-KEY) #(animation/next-frame app)
                    (and (= keyCode Z-KEY) (or ctrlKey metaKey) shiftKey)
                      #(timemachine/do-redo)
                    (and (= keyCode Z-KEY) (or ctrlKey metaKey))
